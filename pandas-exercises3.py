fc0538a31ad8026e45f64b167ae3676153d7c5d6
kamariban <112406892+kamariban@users.noreply.github.com>
2026-08-06 20:56:36 -0400

    Update pandas-exercises3.py

--- pandas-exercises3.py
+++ pandas-exercises3.py
@@ -11,16 +11,19 @@ class EmployeeDatabase:
         self.data = pd.read_csv(self.csv)
         return self.data
 
-
-    def show_all(self, info):
+    def show_all(self):
         df = self.data[["EMPLOYEE_ID", "FIRST_NAME", "LAST_NAME",]]
-        return df
+        print(df)
 
-    def highest_salary(self,info):
+    def highest_salary(self):
         highest = self.data.sort_values(by= "SALARY", ascending=False)
         return highest.iloc[0]
 
-    def average_salary(self,info):
+    def lowest_salary(self):
+        highest = self.data.sort_values(by="SALARY", ascending=True)
+        return highest.iloc[0]
+
+    def average_salary(self):
         counter = 0
         total = 0
         for x in self.data["SALARY"]:
@@ -30,22 +33,39 @@ class EmployeeDatabase:
 
         #return self.data["SALARY"].mean()
 
-    def department_count(self,info):
-        data = self.load_data(info)
-        return dict(data["DEPARTMENT_ID"].value_counts())
+    def department_count(self):
+        return dict(self.data["DEPARTMENT_ID"].value_counts())
 
 
-    def find_employee(self, name):
-        name = self.data[self.data["FIRST_NAME"] == name]
-        return name
+    def find_employee(self, first_name=None, last_name=None, eid=None):
+        if first_name is not None:
+            return self.data[self.data["FIRST_NAME"] == first_name]
+        elif last_name is not None:
+            return self.data[self.data["LAST_NAME"] == last_name]
+        elif eid is not None:
+            return self.data[self.data["EMPLOYEE_ID"] == eid]
+        return None
+
+    def department_report(self, d_id):
+        report = self.data[self.data["DEPARTMENT_ID"] == d_id]
+        d_num = len(report)
+        avg = report["SALARY"].mean().round()
+        sort = report["SALARY"].sort_values(ascending=False)
+        high = sort.iloc[0]
+
+        return (report,
+                f" Department: {d_id}" ,
+                f" Employees: {d_num}" ,
+                f" Average Salary: {avg}" ,
+                f" Highest Salary: {high}")
+
+
 
 test = EmployeeDatabase(file_csv)
-print(test.load_data(file_csv))
+test.load_data(file_csv)
+#test.show_all()
+print(test.department_report(100))
 
-#print(test.load_data(file_csv))
-col = test.load_data(file_csv)
-#print(list(col))
 
-print(test.find_employee("Pat"))
 
 
